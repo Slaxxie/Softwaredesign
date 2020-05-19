35b201943fd4a99117029b6ac6f93f9be9004305
Slaxxie <christianmicka@live.de>
2020-05-19 22:07:42 +0200

    quiz

---
+++ L03/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var L03_Debug;
+(function (L03_Debug) {
+    console.log("Log-Ausgabe");
+    console.error("Error");
+    console.info("info");
+    console.warn("warnung");
+    console.log("hallo", "Welt");
+})(L03_Debug || (L03_Debug = {}));
+//# sourceMappingURL=main.js.map
---
+++ L03/main.ts
@@ -0,0 +1,10 @@
+namespace L03_Debug {
+console.log("Log-Ausgabe");
+console.error("Error");
+console.info("info");
+console.warn("warnung");
+
+console.log("hallo", "Welt");
+
+
+}
